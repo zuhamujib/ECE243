162378d40dc61e20dcb716ca3e92a89a14397fc5
Zuha Mujib <z.mujib1911@yahoo.com>
2024-07-09 21:23:29 +0500

    creating part2_1.s

---
+++ Lab2/part2_1.s
@@ -0,0 +1,62 @@
+.text  # The numbers that turn into executable instructions
+.global _start
+_start:
+
+/* r13 should contain the grade of the person with the student number, -1 if not found */
+/* r10 has the student number being searched */
+	movia r11, result
+	movia r10, 681936		# r10 is where you put the student number being searched for
+
+/* Your code goes here  */
+	movi r12, Snumbers	#holds all the enrolled student numbers
+	movi r13, Grades	#holds the Grades corresponding to each student number
+	
+loop:
+	ldw r9, (r12)	#load the enrolled student numbers
+	ldw r8, (r13)	#load the grades corresponding to current student number
+
+	beq r9, r0, notfound
+	beq r9, r10, found
+	
+	addi r12, r12, 4	#increment to the next student number
+	addi r13, r13, 4	#increment to the next student grades
+	
+	br loop	#branch back to the loop
+found:
+	stw r8, (r11)
+	ldw r6, (r11)	#to see the contents of result
+	#store the contents in r8 into the memory address held by r11 i.e result
+.equ LEDs, 0xFF200000
+	movia r24, LEDs
+	stwio r6, (r24)
+
+	br iloop
+notfound:
+	movi r7, -1
+	stw r7, (r11)
+	ldw r6, (r11)	#to see the contents of result
+	#store -1 in the memory address held by r11 i.e result
+	
+.equ LEDs, 0xFF200000
+	movia r24, LEDs
+	stwio r6, (r24)
+
+iloop: br iloop
+
+.data  	# the numbers that are the data 
+/* result should hold the grade of the student number put into r10, or
+-1 if the student number isn't found */ 
+
+result: .word 0
+		
+/* Snumbers is the "array," terminated by a zero of the student numbers  */
+Snumbers: .word 10392584, 423195, 644370, 496059, 296800
+        .word 265133, 68943, 718293, 315950, 785519
+        .word 982966, 345018, 220809, 369328, 935042
+        .word 467872, 887795, 681936, 0
+
+/* Grades is the corresponding "array" with the grades, in the same order*/
+Grades: .word 99, 68, 90, 85, 91, 67, 80
+        .word 66, 95, 91, 91, 99, 76, 68  
+        .word 69, 93, 90, 72
+	
